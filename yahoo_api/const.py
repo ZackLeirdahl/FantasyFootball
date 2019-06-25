fdbf01bce42819b02df5dfc4cf4795b5f351dd66
ZackLeirdahl <zackleirdahl@gmail.com>
2019-06-25 11:38:13 -0500

    initial merge

---
+++ yahoo_api/const.py
@@ -0,0 +1,3 @@
+oauth_file = r'C:\Users\zleirdahl\Documents\GitHub\FantasyFootball\yahoo_api\oauth2.json'
+league_id = 98349
+
---
+++ yahoo_api/utils.py
@@ -0,0 +1,2 @@
+def format_ids_to_keys(ids):
+    return ['nfl.p.' + str(id) for id in ids]
